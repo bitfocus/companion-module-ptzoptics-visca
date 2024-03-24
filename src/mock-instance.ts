e5e257054ca6d8fc1d86ba7af8c8d0cb1ac874d5
Jeff Walden <jwalden@mit.edu>
2024-03-24 16:57:01 -0700

    chore: Convert commands.js to TypeScript.

--- src/mock-instance.ts
+++ src/mock-instance.ts
@@ -1,6 +1,12 @@
-import type { InstanceStatus, LogLevel } from '@companion-module/base'
+import type { CompanionOptionValues, InstanceStatus, LogLevel } from '@companion-module/base'
+import type { Command } from './visca/command.js'
 
 export type MockInstance = {
 	log: (this: MockInstance, level: LogLevel, msg: string) => void
 	updateStatus: (this: MockInstance, status: InstanceStatus) => void
+	sendCommand: (
+		this: MockInstance,
+		command: Command,
+		options?: CompanionOptionValues | null
+	) => Promise<CompanionOptionValues | null>
 }
