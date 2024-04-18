60cbcef67def3aed73be713508bdb4ec43948b34
Jeff Walden <jwalden@mit.edu>
2024-04-18 16:08:31 -0700

    chore: Limit Jest tests to files with ".(spec|test).ts" suffix in src/.

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,9 @@
 import type { JestConfigWithTsJest } from 'ts-jest';
 
 const config: JestConfigWithTsJest = {
+  // All test files are in src/, so limit Jest to searching for tests there.
+  rootDir: 'src',
+
   // TypeScript files must first be transformed to JS for testing.
   transform: {
     '\\.ts$': ['ts-jest', { useESM: true }]
@@ -22,8 +25,7 @@ const config: JestConfigWithTsJest = {
 
   // The glob patterns Jest uses to detect test files
   testMatch: [
-    "**/__tests__/**/*.[jt]s",
-    "**/?(*.)+(spec|test).[tj]s"
+    "**/?(*.)+(spec|test).ts"
   ],
 
    // An array of regexp pattern strings that are matched against all test
