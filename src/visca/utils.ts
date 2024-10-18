b9ba672b7e13ff0705c2b91b3b86bc9777b6e774
Jeff Walden <jwalden@mit.edu>
2024-10-17 23:31:50 -0700

    chore: Add a `promiseWithResolvers` helper function for future use.

--- src/visca/utils.ts
+++ src/visca/utils.ts
@@ -23,3 +23,23 @@ export function checkBytes(bytes: readonly number[]): void {
 		}
 	}
 }
+
+/** Return an unresolved promise and resolve/reject functions for it. */
+export function promiseWithResolvers<T>(): {
+	promise: Promise<T>
+	resolve: (value: T) => void
+	reject: (reason?: any) => void
+} {
+	let promiseResolve: (value: T) => void
+	let promiseReject: (reason?: any) => void
+	const promise = new Promise<T>((resolve: (value: T) => void, reject: (reason?: any) => void) => {
+		promiseResolve = resolve
+		promiseReject = reject
+	})
+
+	return {
+		promise,
+		resolve: promiseResolve!,
+		reject: promiseReject!,
+	}
+}
