3435545b685764e0937b350747d61bc194f414ff
Jeff Walden <jwalden@mit.edu>
2024-11-27 23:25:55 -0800

    refactor: Rename `actions/actions.ts` to `actions/other-actions.ts`.

--- src/actions/all-actions.ts
+++ src/actions/all-actions.ts
@@ -1,6 +1,6 @@
 import type { ActionDefinitions, PtzOpticsActionId } from './actionid.js'
 import type { PtzOpticsInstance } from '../instance.js'
-import { otherActions } from './actions.js'
+import { otherActions } from './other-actions.js'
 import { presetActions } from './presets.js'
 
 export function getActions(instance: PtzOpticsInstance): ActionDefinitions<PtzOpticsActionId> {
