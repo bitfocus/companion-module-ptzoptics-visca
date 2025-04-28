8b19bd3f36337aa42db7cc025827126bfb8d4f22
Jeff Walden <jwalden@mit.edu>
2025-04-28 14:26:15 -0700

    chore: Add various command parameter/inquiry answer type tests.

--- src/visca/command.test.ts
+++ src/visca/command.test.ts
@@ -1,6 +1,77 @@
+import type { Expect, Equal } from 'type-testing'
 import { describe, expect, test } from 'vitest'
-import { OnScreenDisplayInquiry } from '../camera/osd.js'
-import { ModuleDefinedCommand, UserDefinedCommand } from './command.js'
+import { OnScreenDisplayClose } from '../camera/osd.js'
+import {
+	type Command,
+	type CommandParameters,
+	type CommandParamValues,
+	ModuleDefinedCommand,
+	UserDefinedCommand,
+} from './command.js'
+
+type ParametersForCommand<C extends Command<any>> =
+	C extends Command<infer P> ? (P extends CommandParameters ? CommandParamValues<P> : never) : never
+
+const _NoParameters = new ModuleDefinedCommand([0x81, 0x01, 0x07, 0xff])
+
+type test_NoParameters_paramValues = Expect<
+	Equal<
+		// param values for parameter-less comment are empty
+		ParametersForCommand<typeof _NoParameters>,
+		Record<string, never>
+	>
+>
+
+const _OneParameterNumeric = new ModuleDefinedCommand([0x81, 0x01, 0x02, 0x00, 0x0ff], {
+	parameter: {
+		nibbles: [7],
+	},
+})
+
+type test_OneParameterNumeric_paramValues = Expect<
+	Equal<
+		// param values for one numeric parameter
+		ParametersForCommand<typeof _OneParameterNumeric>,
+		{ parameter: number }
+	>
+>
+
+const _OneParameterTyped = new ModuleDefinedCommand([0x81, 0x01, 0x02, 0x00, 0x0ff], {
+	parameter: {
+		nibbles: [7],
+		convert: (val: string): number => {
+			return Number(val)
+		},
+	},
+})
+
+type test_OneParameterTyped_paramValues = Expect<
+	Equal<
+		// param values for one numeric parameter
+		ParametersForCommand<typeof _OneParameterTyped>,
+		{ parameter: string }
+	>
+>
+
+const _MixedParameters = new ModuleDefinedCommand([0x81, 0x01, 0x02, 0x00, 0x00, 0x0ff], {
+	stringParameter: {
+		nibbles: [7],
+		convert: (val: string): number => {
+			return Number(val)
+		},
+	},
+	numericParameter: {
+		nibbles: [9],
+	},
+})
+
+type test_MixedParameters_paramValues = Expect<
+	Equal<
+		// param values for mixed parameters
+		ParametersForCommand<typeof _MixedParameters>,
+		{ stringParameter: string; numericParameter: number }
+	>
+>
 
 describe('isUserDefined function', () => {
 	test('module-defined command is not user-defined', () => {
@@ -12,7 +83,7 @@ describe('isUserDefined function', () => {
 		expect(udc.isUserDefined()).toBe(true)
 	})
 
-	test('OSD inquiry is not user-defined', () => {
-		expect(OnScreenDisplayInquiry.isUserDefined()).toBe(false)
+	test('OSD close command is not user-defined', () => {
+		expect(OnScreenDisplayClose.isUserDefined()).toBe(false)
 	})
 })
---
+++ src/visca/inquiry.test.ts
@@ -0,0 +1,89 @@
+import type { Equal, Expect } from 'type-testing'
+import { describe, expect, test } from 'vitest'
+import { OnScreenDisplayInquiry } from '../camera/osd.js'
+import { type Answer, type Inquiry, ModuleDefinedInquiry, UserDefinedInquiry } from './inquiry.js'
+
+type AnswerForInquiry<I extends Inquiry<any>> = I extends Inquiry<infer P> ? Answer<P> : never
+
+const _InquiryNoParams = new ModuleDefinedInquiry([0x81, 0x12, 0x34, 0xff], {
+	bytes: [0x90, 0x50, 0x00, 0xff],
+	params: {},
+})
+
+type test_NoParams_answer = Expect<
+	Equal<
+		// answer type with no parameters will be empty
+		keyof AnswerForInquiry<typeof _InquiryNoParams>,
+		never
+	>
+>
+
+type AutoOrManual = 'auto' | 'manual'
+
+const _InquiryOneTypedParam = new ModuleDefinedInquiry([0x81, 0x09, 0x04, 0x38, 0xff], {
+	bytes: [0x90, 0x50, 0x00, 0xff],
+	params: {
+		mode: {
+			nibbles: [5],
+			convert: (param: number): AutoOrManual => {
+				switch (param) {
+					default:
+					case 2:
+						return 'auto'
+					case 3:
+						return 'manual'
+				}
+			},
+		},
+	},
+})
+
+type test_InquiryOneTypedParam_answer = Expect<
+	Equal<
+		// answer type with one typed parameter
+		AnswerForInquiry<typeof _InquiryOneTypedParam>,
+		{ mode: AutoOrManual }
+	>
+>
+
+const _InquiryOneNumericParam = new ModuleDefinedInquiry([0x81, 0x09, 0x04, 0x38, 0xff], {
+	bytes: [0x90, 0x50, 0x00, 0xff],
+	params: {
+		n: {
+			nibbles: [5],
+		},
+	},
+})
+
+type test_InquiryOneNumericParam_answer = Expect<
+	Equal<
+		// answer type with one parameter implicitly numeric
+		AnswerForInquiry<typeof _InquiryOneNumericParam>,
+		{ n: number }
+	>
+>
+
+describe('isUserDefined function', () => {
+	test('module-defined command is not user-defined', () => {
+		const mdi = new ModuleDefinedInquiry([0x81, 0x12, 0x34, 0xff], {
+			bytes: [0x90, 0x50, 0x00, 0xff],
+			params: {},
+		})
+		expect(mdi.isUserDefined()).toBe(false)
+	})
+	test('user-defined command is user-defined', () => {
+		const udc = new UserDefinedInquiry([0x81, 0x12, 0x34, 0xff], {
+			bytes: [0x90, 0x50, 0x00, 0xff],
+			params: {
+				n: {
+					nibbles: [5],
+				},
+			},
+		})
+		expect(udc.isUserDefined()).toBe(true)
+	})
+
+	test('OSD inquiry is not user-defined', () => {
+		expect(OnScreenDisplayInquiry.isUserDefined()).toBe(false)
+	})
+})
